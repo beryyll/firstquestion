c370a4bab517e881dd826171ea6d52b82b7f640d
beryyll <115668645+beryyll@users.noreply.github.com>
2023-11-30 20:54:38 +0300

    Create form.js

---
+++ form.js
@@ -0,0 +1,5 @@
+
+console.log('JS код для валидации формы, отправки данных на сервер и т.д.');
+
+document.querySelector("button").addEventListener("click", () => alert("Успешно"));//.addEventListener("click", () =><a href="https://example.com">Переход</a>);
+
